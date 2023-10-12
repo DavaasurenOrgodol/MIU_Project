d4daaeb8d2c655b9d031865fda09056ccadd90ee
PranjalSharma13 <1SI17CS077@sit.ac.in>
2023-10-12 13:49:52 -0500

    committed

--- src/business/ControllerInterface.java
+++ src/business/ControllerInterface.java
@@ -9,5 +9,6 @@ public interface ControllerInterface {
 	public void addMember(LibraryMember l)throws LibrarySystemException;
 	public void saveBook(Book book);
 	public Book getInfo(String memId, String isbn);
+	public Book getLookUpDetails(String ISBN);
 	public void saveRecord(Checkout record);
 }
